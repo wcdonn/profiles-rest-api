d43924218ecb1df5e7e7ef800f17e27865f34f2e
Donn Carlo Gamboa <donn.gamboa@whitecloak.com>
2017-08-20 13:41:10 +0800

    Added improved permissions to use profile feed items API.

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -8,6 +8,16 @@ class UpdateOwnProfile(permissions.BasePermission):
 
         if request.method in permissions.SAFE_METHODS:
             return True
-        print("object.id", obj.id)
-        print("request.user.id", request.user.id)
+
         return obj.id == request.user.id
+
+class PostOwnStatus(permissions.BasePermission):
+    """Allow users to update their own profile."""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user is trying to update their own status."""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
