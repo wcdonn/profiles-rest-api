d5b0c5638edb9ada175dc40e679d699de04ffe1a
Donn Carlo Gamboa <donn.gamboa@whitecloak.com>
2017-08-20 11:55:14 +0800

    Added ProfileFeedItem, Created migrations and added to django admin

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -1,12 +1,13 @@
 from rest_framework import permissions
 
 class UpdateOwnProfile(permissions.BasePermission):
-    """Allows users to edit their own profile."""
+    """Allow users to edit their own profile."""
 
     def has_object_permission(self, request, view, obj):
-        """Check if user is trying to edit their own profile."""
+        """Check user is trying to edit their own profile."""
 
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        return request.user.id == obj.id
+        print("object.id", obj.id)
+        print("request.user.id", request.user.id)
+        return obj.id == request.user.id
--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -123,3 +123,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser'
+   ),
+}
